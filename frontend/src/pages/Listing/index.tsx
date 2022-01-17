e825c385d67ade66572f4bc35817ada743694dbf
GabrielCrespo <gabriel.crespo1942@gmail.com>
2022-01-16 22:22:34 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,16 @@
+import axios from "axios";
 import MovieCard from "components/MovieCards";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+
+  axios.get(`${BASE_URL}/movies`)
+       .then(response => {
+         console.log(response.data);
+       })
+
+
   return (
     <>
       <Pagination />
